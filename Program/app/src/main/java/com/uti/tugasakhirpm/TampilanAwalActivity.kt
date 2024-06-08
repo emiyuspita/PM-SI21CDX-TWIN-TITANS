90a2eb71d6caef51cf2c1af7e710dbca1782bfa6
emiyuspita <emiyuspita03@gmail.com>
2024-06-08 20:38:20 +0700

    master : Perubahan pada ImageButton Tampilan Awal

--- Program/app/src/main/java/com/uti/tugasakhirpm/TampilanAwalActivity.kt
+++ Program/app/src/main/java/com/uti/tugasakhirpm/TampilanAwalActivity.kt
@@ -1,6 +1,8 @@
 package com.uti.tugasakhirpm
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,18 @@ class TampilanAwalActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_tampilan_awal)
 
+        val btnberanda= findViewById<ImageButton>(R.id.btn_beranda)
+        btnberanda.setOnClickListener {
+            val intent = Intent(this, BerandaActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        val btntransaksi = findViewById<ImageButton>(R.id.btn_transaksi)
+        btntransaksi.setOnClickListener {
+            val intent = Intent(this, BerandaActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
